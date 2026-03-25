8c0dd6cbbfe8330fb6af6cf083ff2d195b0ce944
Jxs <rojasherediajoseemmanuel@gmail.com>
2026-03-25 18:43:47 -0400

    Add test for invalid password login functionality

---
+++ tests/InvalidPasswordLogin.spec.js
@@ -0,0 +1,16 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('Invalid Password Login', async ({ page }) => {
+    // Set the url to the login page
+  await page.goto('https://the-internet.herokuapp.com/login');
+    // Look for the input of Username and Password
+    // Filling the username input with valid username
+  await page.getByLabel("Username").fill("tomsmith")
+    // Fill the password input with invalid password
+  await page.getByLabel("Password").fill("SuperSecretPassword!")
+    // Click the button to login
+  await page.getByRole('button').click();
+  // Invalid Password Message
+  await expect(page.locator("#flash")).toContainText("Your password is invalid! ")
+});
